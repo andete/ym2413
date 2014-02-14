e61119cb34e24e4e0276918bac7497bf6936d55f
Joost Yervante Damad <joost@damad.be>
2014-02-14 15:16:27 +0100

    and add file

---
+++ tool/usart0.h
@@ -0,0 +1,29 @@
+
+namespace usart0 {
+
+  // RX only UART at 31250 baud for MIDI
+  // RX on PE11
+
+  static void setup() {
+
+    // Pin PE11 is configured to Input enabled
+    GPIO->P[4].MODEH = (GPIO->P[4].MODEH & ~_GPIO_P_MODEH_MODE11_MASK) | GPIO_P_MODEH_MODE11_INPUT;
+  
+    USART_InitAsync_TypeDef init = USART_INITASYNC_DEFAULT;
+
+    init.baudrate     = 31250;
+    init.oversampling = usartOVS16;
+    init.databits     = usartDatabits8;
+    init.parity       = usartNoParity;
+    init.stopbits     = usartStopbits1;
+    init.mvdis        = 0;
+    init.prsRxEnable  = 0;
+
+    USART_InitAsync(USART0, &init);
+
+    /* Enable signal RX */
+    USART0->ROUTE |= USART_ROUTE_RXPEN;
+
+  }
+
+}

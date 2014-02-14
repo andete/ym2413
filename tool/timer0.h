60f627446301f815e7d473cd8e8267ed1308d147
Joost Yervante Damad <joost@damad.be>
2014-02-14 15:27:31 +0100

    move timer pin init to timer0.h, although it is probably incorrect for what we want

--- tool/timer0.h
+++ tool/timer0.h
@@ -4,8 +4,16 @@ namespace timer0 {
 
   static void setup() {
 
+    // TODO
+    // Pin PA0 is configured to Input enabled
+  GPIO->P[0].MODEL = (GPIO->P[0].MODEL & ~_GPIO_P_MODEL_MODE0_MASK) | GPIO_P_MODEL_MODE0_INPUT;
+    // Pin PA1 is configured to Input enabled
+  GPIO->P[0].MODEL = (GPIO->P[0].MODEL & ~_GPIO_P_MODEL_MODE1_MASK) | GPIO_P_MODEL_MODE1_INPUT;
+
     // Enable signals CC0, CC1
     TIMER0->ROUTE |= TIMER_ROUTE_CC0PEN | TIMER_ROUTE_CC1PEN;
+
+    // TODO
 
   }
 
